12110d7e614003bc8a415f86179887dc7b55e1dc
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-01-03 12:03:40 +0530

    Add example 3

---
+++ 5-examples/src/Demo3.java
@@ -0,0 +1,53 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        Thread.currentThread().setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("Found Uncaught Exception");
+            }
+        });
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("Let's exit this crazy");
+            }
+        });
+        System.out.println("In: main");
+        try {
+            new Thread(()->{
+                myMethod1();
+            }).start();
+        }catch (Exception e) {
+            System.out.println("Got you");
+        }
+        System.out.println("Out: main");
+    }
+    public static void myMethod1(){
+        System.out.println("In: myMethod1");
+        try {
+            System.out.println(myMethod2(10, 0));
+        }catch (NumberFormatException | ArithmeticException e){
+            System.out.println(new int[]{2,3}[3]);
+        }finally{
+            System.out.println("Time to exit from try");
+        }
+        System.out.println("Out: myMethod1");
+    }
+    public static int myMethod2(int d1, double d2){
+        System.out.println("In: myMethod2");
+        try {
+            return (int) (d1 / d2);
+        }catch (ArithmeticException e){
+            System.out.println("Found: ArithmeticException");
+        }finally{
+            System.out.println("Out: myMethod2");
+            try {
+                return d1 / (int) d2;
+            }catch (NullPointerException e){
+                return d1;
+            }finally {
+                System.out.println("Out: myMethod2");
+            }
+        }
+    }
+}
